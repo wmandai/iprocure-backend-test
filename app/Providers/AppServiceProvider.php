b0e4ad38ab78cb06b3853f30043cffe26e754114
William M <wmandai@rentlipa.com>
2022-10-13 22:18:23 +0300

    - Added search macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Builder::macro(
+            'whereLike',
+            function ($attributes, string $searchTerm) {
+                $this->where(
+                    function (Builder $query) use ($attributes, $searchTerm) {
+                        foreach (Arr::wrap($attributes) as $attribute) {
+                            $query->when(
+                                str_contains($attribute, '.'),
+                                function (Builder $query) use ($attribute, $searchTerm) {
+                                    $buffer = explode('.', $attribute);
+                                    $attributeField = array_pop($buffer);
+                                    $relationPath = implode('.', $buffer);
+                                    $query->orWhereHas(
+                                        $relationPath,
+                                        function (Builder $query) use ($attributeField, $searchTerm) {
+                                            $query->where($attributeField, 'LIKE', "%{$searchTerm}%");
+                                        }
+                                    );
+                                },
+                                function (Builder $query) use ($attribute, $searchTerm) {
+                                    $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
+                                }
+                            );
+                        }
+                    }
+                );
+                return $this;
+            }
+        );
     }
 }
